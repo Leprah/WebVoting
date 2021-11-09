62696895223138180e2fc0e7ff192587a42e6fda
anasembayun <anasembayun02@mail.ugm.ac.id>
2021-11-09 10:39:47 +0700

    push

--- resources/views/kandidat/edit.blade.php
+++ resources/views/kandidat/edit.blade.php
@@ -1,41 +1,108 @@
 @extends('kandidat.layout')
+
+
 @section('content')
-<div class="modal-content">
-    <div class="modal-header">
-        <h5 class="modal-title">Edit Data Kandidat</h5>
-         <h5 class="modal-title" style="display: none;">Edit Data</h5> 
-         <button type="button" data-dismiss="modal" aria-label="Close" class="close">
-             <span aria-hidden="true">×</span></button>
-        </div> 
-        <form>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label>Nama</label>
-                     <input type="text" name="nama" value="{{$data_kandidat->nama}}" class="form-control">
-                     <!---->
-                </div> 
-                <div class="form-group">
-                    <label>Visi</label>
-                    <input type="text" name="visi" value="{{$data_kandidat->visi}}" class="form-control"> 
-                    <!---->
-                </div>
-                <div class="form-group">
-                    <label>Misi</label> 
-                    <input type="text" name="misi" value="{{$data_kandidat->misi}}" class="form-control">
-                    <!---->
-                </div>
-                <div class="form-group">
-                    <label>Foto</label> 
-                    <input type="file" name="image" class="form-control" placeholder="image">
-                    <img src="/image/{{ $data_kandidat->image }}" width="300px">
-                    <!---->
-                </div> 
-                <div class="modal-footer">
-                    <button type="button" data-dismiss="modal" class="btn btn-secondary">Batal</button> 
-                    <button type="submit" class="btn btn-success" style="display: none;">Update</button> 
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </div>
-        </form>
+
+@if (count($errors) > 0)
+
+    <ul class="alert alert-danger">
+
+            @foreach ($errors->all() as $error)
+
+                <li>{{ $error }}</li>
+
+            @endforeach
+
+        </ul>
+
+@endif
+
+<div class="row">
+
+    <div class="col-lg-12 margin-tb">
+
+        <div class="pull-left">
+
+            <h2>Edit Data Kandidat</h2>
+        </div>
     </div>
-                                                    
+
+</div>
+
+
+<form action="/" method="POST" enctype="multipart/form-data">
+
+    @csrf
+
+    
+
+     <div class="row">
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="form-group">
+
+                <strong>Nama:</strong>
+
+                <input type="text" name="judul" value="{{ old('nama') }}" class="form-control" placeholder="nama">
+
+            </div>
+
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="form-group">
+
+            <strong>Visi:</strong>
+
+            <input type="text" name="penulis" value="{{ old('visi') }}" class="form-control" placeholder="visi">
+
+
+            </div>
+
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="form-group">
+
+            <strong>Misi:</strong>
+
+            <input type="text" name="harga" value="{{ old('misi') }}" class="form-control" placeholder="misi">
+
+
+            </div>
+
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="form-group">
+
+            <strong>Foto:</strong>
+
+            <input type="file" name="image" class="form-control" placeholder="image">
+
+
+            </div>
+
+        </div>
+
+        
+
+        <div class="col-xs-12 col-sm-12 col-md-12 text-right">
+
+                <button type="submit" class="btn btn-primary">Simpan</button>
+                
+                <a class="btn btn-primary" href="/buku"> Back</a>
+
+</div>
+
+        </div>
+
+    </div>
+
+     
+
+</form>
+
 @endsection
