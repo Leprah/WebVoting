c5e81287ecb1e5050882c06b589b7aec4aaa8108
aveendazy <zaveenda@mail.ugm.ac.id>
2021-11-30 17:55:29 +0700

    ubah statistik

--- resources/views/webVote/master.blade.php
+++ resources/views/webVote/master.blade.php
@@ -117,10 +117,7 @@
 
 
     <!-- Header -->
-    <div class="container-fluid">
-      <br>
-      <br>
-    </div>
+    
    
     <!-- Page content -->
     
@@ -213,7 +210,87 @@
         }]
     });
     </script>
-  
+
+    <!-- Chart Batang -->
+
+    <script>
+      var chart = Highcharts.chart('batang', {
+
+      chart: {
+          type: 'column'
+      },
+
+      title: {
+          text: 'Voter Berdasarkan Angkatan'
+      },
+
+      legend: {
+          align: 'right',
+          verticalAlign: 'middle',
+          layout: 'vertical'
+      },
+
+      xAxis: {
+          categories: ['Angkatan 2019', 'Angkatan 2020', 'Angkatan 2021'],
+          labels: {
+              x: -10
+          }
+      },
+
+      yAxis: {
+          allowDecimals: false,
+          title: {
+              text: 'Jumlah Pemilih'
+          }
+      },
+
+      series: [{
+          name: 'Ana',
+          data: [5, 10, 15]
+      }, {
+          name: 'Alief',
+          data: [15, 10, 5]
+      }, {
+          name: 'Aveenda',
+          data: [10, 10, 10]
+      }],
+
+      responsive: {
+          rules: [{
+              condition: {
+                  maxWidth: 500
+              },
+              chartOptions: {
+                  legend: {
+                      align: 'center',
+                      verticalAlign: 'bottom',
+                      layout: 'horizontal'
+                  },
+                  yAxis: {
+                      labels: {
+                          align: 'left',
+                          x: 0,
+                          y: -5
+                      },
+                      title: {
+                          text: null
+                      }
+                  },
+                  subtitle: {
+                      text: null
+                  },
+                  credits: {
+                      enabled: false
+                  }
+              }
+          }]
+      }
+      });
+
+    </script>
+
+
+
 </body>
 
 </html>
