06d62229b8170e41c2a7af3287d0532ff6b80ce2
Alief Rahman <aliefrahman02@mail.ugm.ac.id>
2021-12-11 15:05:49 +0700

    voting

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,15 +6,20 @@ use Illuminate\Http\Request;
 use App\Kandidat;
 use App\Pemilih;
 use App\Pengaturan;
+use App\Voting;
 
 class IndexController extends Controller
 {
     public function index(){
         $batas = 3;
         $pengaturan = Pengaturan::first(); 
+        $jumlah_pemilih = Pemilih::all()->count(); 
+        $jumlah_kandidat = kandidat::all()->count();
+        $jumlah_voting = Voting::all()->count();
+        $belum_voting = $jumlah_pemilih - $jumlah_voting;
         $kandidat = Kandidat::orderBy('no_urut', 'asc')->paginate($batas);
         $no = $batas * ($kandidat->currentPage() - 1);
-        return view('webVote.index' , ['kandidat' => $kandidat, 'pengaturan' => $pengaturan]);
+        return view('webVote.index' , compact('kandidat', 'pengaturan' , 'jumlah_pemilih', 'jumlah_kandidat', 'jumlah_voting', 'belum_voting'));
     }
 
 }
--- database/migrations/2021_12_11_065935_create_voting.php
+++ database/migrations/2021_12_11_065935_create_voting.php
@@ -16,12 +16,10 @@ class CreateVoting extends Migration
         Schema::create('voting', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('kandidat_id')->unsigned();
-            $table->unsignedBigInteger('pemilih_id')->unsigned();
             $table->unsignedBigInteger('user_id')->unsigned();
             $table->integer('status')->unsigned();
             
             $table->foreign('user_id')->references('id')->on('users')->onDetele('restrict');
-            $table->foreign('pemilih_id')->references('id')->on('pemilih')->onDetele('cascade');
             $table->foreign('kandidat_id')->references('id')->on('kandidat')->onDetele('cascade');
             $table->timestamps();
         });
